307a827a3210a3aae7b4389169483bd3e56b4a44
ViktoriaMlechko <viktoriamlechko@yandex.ru>
2021-06-07 05:21:09 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void enter(int*, int*);
+
 int main()
 {
+	int num1, num2;
+	
+	enter(&num1, &num2);
 	
 	return 0;
+}
+
+void enter(int *num1, int *num2)
+{
+	cout << "введите первое число: ";
+	cin >> num1;
+	
+	cout << "введите второе число: ";
+	cin >> num2;
 }
